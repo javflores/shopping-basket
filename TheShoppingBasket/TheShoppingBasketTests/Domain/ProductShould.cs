98cf35b3ecb75f57e412953127187a6684c723c9
javflores <jflores@findmypast.com>
2016-05-17 08:02:29 +0100

    Better way to test abstract Product functionality. Don't create a child but use mock object.

--- TheShoppingBasket/TheShoppingBasketTests/Domain/ProductShould.cs
+++ TheShoppingBasket/TheShoppingBasketTests/Domain/ProductShould.cs
@@ -1,4 +1,6 @@
-﻿using TheShoppingBasket.Domain.Product;
+﻿using NSubstitute;
+using TheShoppingBasket.Domain;
+using TheShoppingBasket.Domain.Product;
 using Xunit;
 
 namespace TheShoppingBasketTests.Domain
@@ -8,14 +10,14 @@ namespace TheShoppingBasketTests.Domain
         [Fact]
         public void initialize_with_quantity_zero()
         {
-            Product product = new Bread();
+            Product product = Substitute.For<Product>();
             Assert.Equal(0, product.Quantity);
         }
 
         [Fact]
         public void update_quantity_when_adding_more()
         {
-            Product product = new Bread();
+            Product product = Substitute.For<Product>();
 
             product.AddQuantity(2);
 
@@ -25,7 +27,8 @@ namespace TheShoppingBasketTests.Domain
         [Fact]
         public void cost_price_times_quantity()
         {
-            Product product = new Bread();
+            Product product = Substitute.For<Product>();
+            product.Price.Returns(new Money(1.0m));
 
             product.AddQuantity(2);
 
